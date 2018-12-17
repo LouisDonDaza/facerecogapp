b084ef96f55eb4a0bb2bc1d49644d609234ae514
Louis Daza <louisdaza@hotmail.com>
2018-12-17 08:31:24 -0800

    Updates

---
+++ precache-manifest.f6ad81833754f72f9a09a9b6709fdc1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15c6f39dbaab4bdb14748863c3a8b39",
+    "url": "/LouisDonDaza/facerecogapp/static/media/brain.b15c6f39.png"
+  },
+  {
+    "revision": "55244c440c4cf7a9fc40",
+    "url": "/LouisDonDaza/facerecogapp/static/js/runtime~main.55244c44.js"
+  },
+  {
+    "revision": "1637c72d1bc555016a60",
+    "url": "/LouisDonDaza/facerecogapp/static/js/main.1637c72d.chunk.js"
+  },
+  {
+    "revision": "9105888a1fff4f8eab4b",
+    "url": "/LouisDonDaza/facerecogapp/static/js/1.9105888a.chunk.js"
+  },
+  {
+    "revision": "1637c72d1bc555016a60",
+    "url": "/LouisDonDaza/facerecogapp/static/css/main.5dcfdc81.chunk.css"
+  },
+  {
+    "revision": "9105888a1fff4f8eab4b",
+    "url": "/LouisDonDaza/facerecogapp/static/css/1.01d81e46.chunk.css"
+  },
+  {
+    "revision": "f31ac39803aabf1b7438c09152b58d50",
+    "url": "/LouisDonDaza/facerecogapp/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/LouisDonDaza/facerecogapp/precache-manifest.f6ad81833754f72f9a09a9b6709fdc1b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/LouisDonDaza/facerecogapp/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
